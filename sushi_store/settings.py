f423d94d84bdaf6a378638f6f3201167c0e42306
Oleg Didechkin <frikol3000@gmail.com>
2021-09-21 23:39:00 +0300

    Middleware updated

--- sushi_store/settings.py
+++ sushi_store/settings.py
@@ -92,6 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
