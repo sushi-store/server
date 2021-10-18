e72d805df0c5b202b91a474cac36a9abdd48bc43
Oleg Didechkin <frikol3000@gmail.com>
2021-10-18 20:16:24 +0300

    Url issue fixed

--- order_api/urls.py
+++ order_api/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'order_api'
 
 urlpatterns = [
-    path('/', views.OrdersList.as_view()),
+    path('', views.OrdersList.as_view()),
     path('create/', views.CreateOrder.as_view()),
     path('create-by-uuid/', views.CreateTempOrder.as_view()),
     path('<uuid>/', views.OrderDetailUUId.as_view()),
